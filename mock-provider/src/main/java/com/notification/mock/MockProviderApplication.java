2d0f95aac7e9ecb744286578b12ab7b3ca6de8e1
yuna1313 <yuna38748435@gmail.com>
2026-08-06 17:45:59 +0900

    mock-provider 프로젝트 초기 세팅

---
+++ mock-provider/src/main/java/com/notification/mock/MockProviderApplication.java
@@ -0,0 +1,13 @@
+package com.notification.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockProviderApplication.class, args);
+	}
+
+}
---
+++ mock-provider/src/test/java/com/notification/mock/MockProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
